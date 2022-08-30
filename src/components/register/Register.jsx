5872f5ade32a171e31c11ee3122fc2552785de9d
FelipeV99 <38900312+FelipeV99@users.noreply.github.com>
2022-08-30 07:33:13 -0500

    comment

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -44,7 +44,7 @@ const Register = () => {
             <div className='register-container'>
                 <img src={olabLogo} alt="Olab-logo" className='olab-logo' />
                 <h2>Register</h2>
-                <p className='normal-p intro-p'>  fill out the following fields in order to start using O-lab.</p>
+                <p className='normal-p intro-p'>Please fill out the following fields in order to start using O-lab.</p>
                 <form ref={formRef} onSubmit={sendEmail} className='form-register'>
                     <label htmlFor="fullname">Full Name *</label>
                     <input name="fullname" type="text" required />
