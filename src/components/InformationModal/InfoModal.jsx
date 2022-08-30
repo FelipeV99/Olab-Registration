968514cda543fd2901afc5f211be1d35fce76624
FelipeV99 <38900312+FelipeV99@users.noreply.github.com>
2022-08-29 23:33:01 -0500

    date

--- src/components/InformationModal/InfoModal.jsx
+++ src/components/InformationModal/InfoModal.jsx
@@ -11,7 +11,7 @@ const InfoModal = (props) => {
             <div className='overlay-style'>
                 <div className="modal-style">
                     <BiInfoCircle className='info-icon' size={22} />
-                    <p className='overlay-p'>In 1 hour you'll have access to O-lab with your registered email and/or phone number, your password will be 12345</p>
+                    <p className='overlay-p'>In 1 hour you'll have access to O-lab with your registered email and/or phone number, your password will be 12345. If you registered with your email, check your inbox to see these credentials</p>
                     <button onClick={props.close} className='btn' id='overlay-btn'>I Understand</button>
                 </div>
             </div>
